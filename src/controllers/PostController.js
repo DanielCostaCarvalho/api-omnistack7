da230680d8be573a94aa23fbfacaea4bd3a0033b
Daniel Carvalho <danielcostacarvalho@gmail.com>
2019-06-18 14:04:32 -0300

    redimensionando as imagens

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -1,4 +1,7 @@
 const Post = require('../models/Post');
+const sharp = require('sharp');
+const path = require('path');
+const fs = require('fs');
 
 module.exports = {
     async index(req, res){
@@ -8,6 +11,13 @@ module.exports = {
     async store(req, res){        
         const {autor, lugar, descricao, hashtag} = req.body;
         const {filename: imagem} = req.file;
+
+        await sharp(req.file.path)
+        .resize(500)
+        .jpeg({ quality: 70})
+        .toFile(
+            path.resolve(req.file.destination, 'redimensionado', image)
+        )
 
         const post = await Post.create({
             autor,
