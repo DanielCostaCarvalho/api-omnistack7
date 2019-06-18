6c985db29defd27036cc55ca644330afa02e97c4
Daniel Costa Carvalho <danielcostacarvalho@gmail.com>
2019-06-18 11:28:37 -0300

    Salvar likes e listar posts

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,13 @@ const express = require('express');
 const multer = require('multer');
 const PostController = require('./controllers/PostController');
 const uploadConfig = require('./config/upload');
+const LikeController = require('./controllers/LikeController');
 
 const upload = multer(uploadConfig);
 const routes = new express.Router();
 
 routes.post('/posts', upload.single('imagem'), PostController.store);
+routes.get('/posts', PostController.index);
+routes.post('/posts/:id/like', LikeController.store);
 
 module.exports = routes;
