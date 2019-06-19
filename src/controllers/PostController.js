045200e6ce5015a765b2c6dde0423e5bd349ea4f
Daniel Carvalho <danielcostacarvalho@gmail.com>
2019-06-19 17:12:11 -0300

    Acrescentando o socket.io e consertando o cors

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -1,38 +1,38 @@
-const Post = require('../models/Post');
-const sharp = require('sharp');
-const path = require('path');
-const fs = require('fs');
+const Post = require("../models/Post");
+const sharp = require("sharp");
+const path = require("path");
+const fs = require("fs");
 
 module.exports = {
-    async index(req, res){
-        const posts = await Post.find().sort('-createdAt');
-        return res.json(posts);
-    },
-    async store(req, res){        
-        const {autor, lugar, descricao, hashtag} = req.body;
-        const {filename: imagem} = req.file;
+  async index(req, res) {
+    const posts = await Post.find().sort("-createdAt");
+    return res.json(posts);
+  },
+  async store(req, res) {
+    const { autor, lugar, descricao, hashtag } = req.body;
+    const { filename: imagem } = req.file;
 
-        const [name] = imagem.split('.');
+    const [name] = imagem.split(".");
 
-        const fileName = `${name}.jpg`
+    const fileName = `${name}.jpg`;
 
-        await sharp(req.file.path)
-        .resize(500)
-        .jpeg({ quality: 70})
-        .toFile(
-            path.resolve(req.file.destination, 'redimensionado', fileName)
-        )
+    await sharp(req.file.path)
+      .resize(500)
+      .jpeg({ quality: 70 })
+      .toFile(path.resolve(req.file.destination, "redimensionado", fileName));
 
-        fs.unlinkSync(req.file.path);
+    fs.unlinkSync(req.file.path);
 
-        const post = await Post.create({
-            autor,
-            lugar,
-            descricao,
-            hashtag,
-            fileName
-        });
+    const post = await Post.create({
+      autor,
+      lugar,
+      descricao,
+      hashtag,
+      fileName
+    });
 
-        return res.json(post);
-    }
-}
+    req.io.emit("post", post);
+
+    return res.json(post);
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
 
 const app = express();
 
-mongoose.connect('mongodb+srv://rocketTeste:FCStzaX1YmT7SjxV@cluster0-dbich.mongodb.net/test?retryWrites=true&w=majority',
-{useNewUrlParser: true}
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+mongoose.connect(
+  "mongodb+srv://rocketTeste:FCStzaX1YmT7SjxV@cluster0-dbich.mongodb.net/test?retryWrites=true&w=majority",
+  { useNewUrlParser: true }
 );
+
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
 
 app.use(cors());
 
-app.use('/files', express.static(path.resolve(_dirname, '..', 'uploads', 'redimensionado')));
+app.use(
+  "/files",
+  express.static(path.resolve(_dirname, "..", "uploads", "redimensionado"))
+);
 
-app.use(require('./routes'));
+app.use(require("./routes"));
 
-app.listen(3333);
+server.listen(3333);
